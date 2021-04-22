c936acbbbf2205c1e9f1c2121c85b183292c744e
Alexandre Gabriel <alexandregabrielc.silva@gmail.com>
2021-04-22 19:30:52 -0300

    New Kata

---
+++ js/StringRepeat.js
@@ -0,0 +1,18 @@
+//My firts solution:
+
+/* const repeatStr = (repNum, char) => {
+    let acc = char;
+    for (let i = 1; i < repNum; i++){
+        acc += char
+    }
+    return acc
+}
+
+console.log(repeatStr(6, "I")); */
+
+// Cleverest solution (Using repeat(n))
+
+const repeatStr = (repNum, char) => {
+    return char.repeat(repNum)
+}
+console.log(repeatStr(6, "I"))
